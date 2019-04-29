4cb2de94294d6a0de1240cc8997d111184a84425
Christian <Christian@gutchc15>
2019-04-29 13:35:29 +0200

    HelloWorld klasse Test

---
+++ src/main/java/chif15/adressvalidierungprojekt/testklasse3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chif15.adressvalidierungprojekt;
+
+/**
+ *
+ * @author Christian
+ */
+public class testklasse3 {
+    //HelloWorld by gutchc15
+}
