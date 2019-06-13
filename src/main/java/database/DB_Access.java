85549633bf0f41b9f48f42a5f25928a69011f42b
marinaspari <marinaspari@10.151.87.23>
2019-06-13 08:24:56 +0200

    ois oasch oida

--- src/main/java/database/DB_Access.java
+++ src/main/java/database/DB_Access.java
@@ -15,80 +15,91 @@ import java.sql.Statement;
  *
  * @author Christian
  */
-public class DB_Access {
+public class DB_Access
+{
 
     private static DB_Access theInstance;
 
-    public static DB_Access getInstance() {
-        if (theInstance == null) {
+    public static DB_Access getInstance()
+    {
+        if (theInstance == null)
+        {
             theInstance = new DB_Access();
         }
         return theInstance;
     }
 
-    private DB_Access() {
+    private DB_Access()
+    {
     }
     private DB_StatementPool stmtPool = DB_StatementPool.getInstance();
 
-    public void insertAddress(Address address) throws SQLException {
+    public void insertAddress(Address address) throws SQLException
+    {
         PreparedStatement insertAddress = stmtPool.getPreparedStatement(DB_StatementType.INSERT_ADDRESS);
         Statement stat = stmtPool.getStatement();
         insertAddress.setString(1, address.getStreet());
         insertAddress.setInt(2, address.getHouseNr());
         int zipCode = 0;
         int region = 0;
-        String country="";
-        ResultSet rs = stat.executeQuery("SELECT ZipCode FROM ZipCode WHERE ZipCode LIKE '"+address.getZipCode()+"';");
-        while (rs.next()) {
+        String country = "";
+        ResultSet rs = stat.executeQuery("SELECT ZipCode FROM ZipCode WHERE ZipCode LIKE '" + address.getZipCode() + "';");
+        while (rs.next())
+        {
             zipCode = rs.getInt("ZipCode");
         }
-        if (zipCode == 0) {
+        if (zipCode == 0)
+        {
             zipCode = address.getZipCode();
-        } else {
+        }
+        else
+        {
             stat.executeQuery("INSERT INTO ZipCode VALUES(" + address.getZipCode() + "," + address.getCity() + ");");
         }
         insertAddress.setInt(3, zipCode);
         insertAddress.setString(4, address.getCity());
-        rs = stat.executeQuery("SELECT * FROM Region WHERE RegionName LIKE '"+address.getRegion()+"';");
-        
-        while(rs.next())
+        rs = stat.executeQuery("SELECT * FROM Region WHERE RegionName LIKE '" + address.getRegion() + "';");
+
+        while (rs.next())
         {
             region = rs.getInt("RegionID");
         }
-        if(region==0)
+        if (region == 0)
         {
             region = 1;
         }
-        else{
-            stat.executeQuery("INSERT INTO Region(RegionID,RegionName) VALUES("+region+";" + address.getRegion()+ ");");
+        else
+        {
+            stat.executeQuery("INSERT INTO Region(RegionID,RegionName) VALUES(" + region + ";" + address.getRegion() + ");");
         }
-        
+
         insertAddress.setInt(5, region);
-        rs = stat.executeQuery("SELECT * FROM Country WHERE CountryLong LIKE '"+address.getCountry()+"';");
-        while(rs.next())
+        rs = stat.executeQuery("SELECT * FROM Country WHERE CountryLong LIKE '" + address.getCountry() + "';");
+        while (rs.next())
         {
             country = rs.getString("CoutryLong");
         }
-        if(country.isEmpty())
+        if (country.isEmpty())
         {
             country = address.getCountry();
         }
-        else{
-            stat.executeQuery("INSERT INTO Coutry VALUES(" + address.getCountry()+ ");");
+        else
+        {
+            stat.executeQuery("INSERT INTO Coutry VALUES(" + address.getCountry() + ");");
         }
         insertAddress.setString(6, country);
         insertAddress.execute();
-        
-        System.out.println("\n\n####"+insertAddress.toString()+"###\n\n");
     }
 
-    public String getAllTables() throws SQLException {
+    public String getAllTables() throws SQLException
+    {
         String out = "";
 
         Statement stat = stmtPool.getStatement();
         String sqlString = "select table_name from information_schema.tables";
         ResultSet rs = stat.executeQuery(sqlString);
-        while (rs.next()) {
+        while (rs.next())
+        {
             String table = rs.getString("table_name");
             out += table + "\n";
         }
@@ -97,12 +108,16 @@ public class DB_Access {
         return out;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         DB_Access dba = DB_Access.getInstance();
-        try {
+        try
+        {
             String out = dba.getAllTables();
             System.out.println(out);
-        } catch (SQLException ex) {
+        }
+        catch (SQLException ex)
+        {
             System.out.println(ex.toString());
         }
     }
---
+++ src/main/webapp/unexpectedError.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : successfullySaved
+    Created on : Jun 11, 2019, 2:52:51 PM
+    Author     : marinaspari
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Success!</title>
+    </head>
+    <body>
+        <form method="GET" action="ValidationServlet">
+            <h1>Unexpected Error...</h1>
+            <input type="submit" value="back">
+        </form>
+    </body>
+</html>
